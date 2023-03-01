e02631b79650d9c5fa1ec62840ddbf1c35713c3d
PhanThanhVinh <phanthanhvinh22121996@gmail.com>
2023-03-01 19:59:48 +0700

    fe31-git

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
+import Main from "./Main";
+import Footer, { name, className } from "./Footer";
+
+function App() {
+  const abc = "abc";
+  return (
+    <div className="wrapper">
+      <Header />
+      <Main />
+      <Footer />
+      {name} - {className}
+    </div>
+  );
+}
+
+export default App;
